cf2ca953644beecd80372d60cd1712e22fc7032a
doblon8 <doblon8@proton.me>
2025-06-06 19:12:42 +0200

    Add QRCodeGenerator utility class for generating and saving QR codes

---
+++ src/main/java/com/github/doblon8/jzbar/utils/QRCodeGenerator.java
@@ -0,0 +1,32 @@
+package com.github.doblon8.jzbar.utils;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import com.google.zxing.*;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+public class QRCodeGenerator {
+
+    public static BufferedImage generateQRCodeImage(String text, int size) throws WriterException {
+        QRCodeWriter qrWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrWriter.encode(text, BarcodeFormat.QR_CODE, size, size);
+
+        BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_BYTE_GRAY);
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                int grayValue = bitMatrix.get(x, y) ? 0 : 255; // black=0, white=255
+                int rgb = (grayValue << 16) | (grayValue << 8) | grayValue;
+                image.setRGB(x, y, rgb);
+            }
+        }
+        return image;
+    }
+
+    public static void saveQRCodeImage(BufferedImage image, String filePath) throws Exception {
+        File outputFile = new File(filePath);
+        ImageIO.write(image, "png", outputFile);
+    }
+}
+
