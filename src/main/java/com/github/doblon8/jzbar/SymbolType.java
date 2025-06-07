a02883a5a5ac1f40a181714c2ccf8b5889d05899
doblon8 <doblon8@proton.me>
2025-06-07 06:11:22 +0200

    Add SymbolType class with barcode constants

---
+++ src/main/java/com/github/doblon8/jzbar/SymbolType.java
@@ -0,0 +1,30 @@
+package com.github.doblon8.jzbar;
+
+import static com.github.doblon8.jzbar.bindings.zbar.*;
+
+public class SymbolType {
+    public static final int ADDON = ZBAR_ADDON();
+    public static final int ADDON2 = ZBAR_ADDON2();
+    public static final int ADDON5 = ZBAR_ADDON5();
+    public static final int CODABAR = ZBAR_CODABAR();
+    public static final int CODE128 = ZBAR_CODE128();
+    public static final int CODE39 = ZBAR_CODE39();
+    public static final int CODE93 = ZBAR_CODE93();
+    public static final int COMPOSITE = ZBAR_COMPOSITE();
+    public static final int DATABAR = ZBAR_DATABAR();
+    public static final int DATABAR_EXP = ZBAR_DATABAR_EXP();
+    public static final int EAN13 = ZBAR_EAN13();
+    public static final int EAN2 = ZBAR_EAN2();
+    public static final int EAN5 = ZBAR_EAN5();
+    public static final int EAN8 = ZBAR_EAN8();
+    public static final int I25 = ZBAR_I25();
+    public static final int ISBN10 = ZBAR_ISBN10();
+    public static final int ISBN13 = ZBAR_ISBN13();
+    public static final int NONE = ZBAR_NONE();
+    public static final int PARTIAL = ZBAR_PARTIAL();
+    public static final int PDF417 = ZBAR_PDF417();
+    public static final int QRCODE = ZBAR_QRCODE();
+    public static final int SQCODE = ZBAR_SQCODE();
+    public static final int UPCA = ZBAR_UPCA();
+    public static final int UPCE = ZBAR_UPCE();
+}
