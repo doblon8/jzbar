4aff98d8264d9c9950b76299b66b23051e079443
doblon8 <doblon8@proton.me>
2025-10-11 16:22:56 +0200

    Replace hardcoded paths with environment variables

--- generate-bindings.sh
+++ generate-bindings.sh
@@ -1,9 +1,12 @@
+# Configure these paths according to your environment
 export JEXTRACT_HOME=/tmp/jextract-22
+export INCLUDE_DIR=/usr/include
+export LIB_PATH=/usr/lib/x86_64-linux-gnu/libzbar.so
 
 ${JEXTRACT_HOME}/bin/jextract \
       --output src/main/java \
       --target-package io.github.doblon8.jzbar.bindings \
       --header-class-name zbar \
-      --library /usr/lib/x86_64-linux-gnu/libzbar.so \
-      --include-dir /usr/include/zbar \
-      /usr/include/zbar.h
+      --library ${LIB_PATH} \
+      --include-dir ${INCLUDE_DIR}/zbar \
+      ${INCLUDE_DIR}/zbar.h
